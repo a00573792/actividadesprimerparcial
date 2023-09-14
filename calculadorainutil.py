3705dc352c4c274d6249b8b60b23fc028d31e643
a00573792 <145036367+a00573792@users.noreply.github.com>
2023-09-14 11:46:14 -0600

    Create calculadorainutil.py

---
+++ calculadorainutil.py
@@ -0,0 +1,10 @@
+def calculadora_inutil():
+  
+  print("Inserta un número para usar como base")
+  numero = input()
+  print("Dame otro número diferente para usar como exponente")
+  nomero = input()
+  operacion = int(numero + nomero)+10
+  print("resultado=",operacion)
+
+calculadora_inutil()
