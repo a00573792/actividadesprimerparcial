9301adb95b5f25a433507a948c15af62e737edef
a00573792 <145036367+a00573792@users.noreply.github.com>
2023-09-14 11:43:49 -0600

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def calculadora():
+
+  print ("¿Qué quiere de comer?")
+  comer= input()
+  print (comer,"cuesta 90 pesos, la propina viene incluida en el precio")
+  print ("su comida cuesta 90 pesos mas 10% de propina que son 9 pesos es 99 pesos")
+  
+calculadora()
