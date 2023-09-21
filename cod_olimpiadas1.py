259c847d02c1e6243e3f8040fe53235e130e8b40
a00573792 <145036367+a00573792@users.noreply.github.com>
2023-09-21 09:21:45 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,36 @@
+def multiplicacion (a,b):
+  x = a * b
+  return x
+def division (a,b):
+  x = a / b
+  return x
+  print ("dame el primer número")
+  a = (input())
+  print ("dame el segundo número")
+  b = (input())
+  print ("tus resultados son", multiplicacion, division)
+
+def conversion (k,m):
+  k = m * 1000
+  return k
+  print ("¿Cuántos kilometros recorriste")
+  k = input()
+  m = int(k) * int(1000)
+  print ("recorriste en metros", )
+  
+def conversion()
+
+
+def area_triangulo(b,h):
+  a = b * h
+  return a
+  print ("dame una base")
+  base = input()
+  print ("dame una altura")
+  altura = input()
+  operacion = int(base) * int(altura) / 2
+  print ("tu area es", operacion)
+
+def area_triangulo(
+
+
